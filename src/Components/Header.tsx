2fa51f7d7cdeb161e17edbf653e0e05ee0f7e632
Cristopher <cristophervga@hotmail.com>
2023-10-13 14:47:44 -0300

    Estilização do Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { FiShoppingCart } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-slate-200 w-full px-1'>
+      <nav className='h-14 w-full max-w-7xl flex justify-between items-center'>
+        <Link to="/"> 
+          DevShop
+        </Link>
+        <Link className='relative' to="/cart">
+          <FiShoppingCart size={24} color="#121212"/>
+          <span className='absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-6 h-6 flex items-center justify-center text-white'>
+            2
+          </span>
+        </Link>
+      </nav>
+    </header>
+
   )
 }
 
